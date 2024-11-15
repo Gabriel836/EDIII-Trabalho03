105a0e3f28ff9bff192b84284a3f1e756fd2ee5a
Matheus-Cavalcanti <matheus.c.cavalcanti@hotmail.com>
2024-11-15 19:35:05 -0300

    tad inicial

---
+++ src/headers/grafo.h
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_STR 100
+
+//Estrutura para representar um animal
+typedef struct Animal {
+    char nome[MAX_STR];
+    char especie[MAX_STR];
+    char habitat[MAX_STR];
+    char dieta[MAX_STR];
+    char tipo[MAX_STR];
+} Animal;
+
+//Estrutura para representar uma aresta (conexão)
+typedef struct Aresta {
+    Animal* destino;
+    int peso;
+    struct Aresta* prox;
+} Aresta;
+
+//Estrutura para representar um vértice
+typedef struct Vertice {
+    Animal* animal;
+    int grauEntrada;
+    int grauSaida;
+    Aresta* listaArestas; //Lista encadeada de arestas
+    struct Vertice* prox;
+} Vertice;
+
+//Estrutura para representar um grafo
+typedef struct Grafo {
+    Vertice* listaVertices; //Lista encadeada de vértices
+} Grafo;
+
+Grafo* criarGrafo();
+Animal* criarAnimal(const char* nome, const char* especie, const char* habitat, const char* dieta, const char* tipo);
+Vertice* criarVertice(Animal* animal);
+void adicionarVertice(Grafo* grafo, Animal* animal);
+void adicionarAresta(Grafo* grafo, const char* origem, const char* destino, int peso);
+void imprimirGrafo(Grafo* grafo);
